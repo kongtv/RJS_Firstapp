9d2b94efe49d019deccfb06787fb7ad95a5032b8
kongtv <kong54170@gmail.com>
2020-11-26 15:58:44 +0700

    first commit

---
+++ src/FirstPage.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class FirstPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>แนะนำตัว</h1>
+                <h3>รหัส 6239010006</h3>
+                <h3>วทัญญู จินะโสติ</h3>
+                <h3>สาขา เทคโนโลยีสารสนเทศ</h3>
+                <h3>ชั้น ปวส.2</h3>
+                <h3>วิทยาลัยการอาชีพสอง</h3>
+            </div>
+        );
+    }
+}
+
+export default FirstPage;
